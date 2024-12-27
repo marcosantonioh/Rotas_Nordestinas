c0fd0198a98bc1ca661ed85cca9bf35011fbee41
Marcos <onetestt6@gmail.com>
2024-12-27 10:07:15 -0300

    inicio no side-bar lateral

--- js/cidades.js
+++ js/cidades.js
@@ -2,13 +2,25 @@
 const menuButton = document.getElementById('menuButton');
 const closeButton = document.getElementById('closeButton');
 const userSidebar = document.getElementById('userSidebar');
+const userSidebarWrapper = document.getElementById('userSidebarWrapper');
 
-// Adiciona o evento de clique no botão do menu
+// Abre o modal ao clicar no botão do menu
 menuButton.addEventListener('click', () => {
+    userSidebarWrapper.classList.add('open');
     userSidebar.classList.add('open');
 });
 
-// Adiciona o evento de clique no botão de fechar
+// Fecha o modal ao clicar no botão de fechar
 closeButton.addEventListener('click', () => {
+    userSidebarWrapper.classList.remove('open');
     userSidebar.classList.remove('open');
+});
+
+// Fecha o modal ao clicar fora dele
+userSidebarWrapper.addEventListener('click', (event) => {
+    // Verifica se o clique foi fora do modal
+    if (!userSidebar.contains(event.target)) {
+        userSidebarWrapper.classList.remove('open');
+        userSidebar.classList.remove('open');
+    }
 });
